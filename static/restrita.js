094d508e794e83c36d7752698c24d15f4597d76d
Marcella Dornelas <marcelladornelas@MacBook-Air-de-Marcella.local>
2018-08-31 17:42:35 -0300

    FixDropdown

--- static/restrita.js
+++ static/restrita.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
-    if ( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-        $('.dropdown-trigger').dropdown({ hover: true, constrainWidth: false });
-    } else {
-        $('.dropdown-trigger').dropdown();
-    }
 });
+
+if ( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    $('.dropdown-trigger').dropdown({constrainWidth: false});
+} else {
+    $('.dropdown-trigger').dropdown({ hover: true, constrainWidth: false });
+}
